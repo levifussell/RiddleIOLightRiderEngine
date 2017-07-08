01b9618e6faf98f303896037d624355ad72571ce
Thais Minet <thais@minet.dk>
2017-07-08 13:44:02 -0700

    moved all java bots into JavaBots folder

---
+++ Bots/JavaBots/Bot.java
@@ -0,0 +1,86 @@
+import java.util.Scanner;
+
+
+public abstract class Bot {
+    private Scanner scan = new Scanner(System.in);
+    // settings variables
+    protected int timeBank;
+    protected int timePerMove;
+    protected String playerNames;
+    protected String yourBot;
+    protected int yourBotId;
+    protected int fieldWidth;
+    protected int fieldHeight;
+
+    // game variables
+    protected int gameRound;
+    protected String gameField;
+
+    abstract String getAction();
+
+    public void setBotId(int id) {
+        this.yourBotId = id;
+    }
+    
+    public void updateSettings(String type, String value) {
+        switch (type) {
+        case "timebank":
+            this.timeBank = Integer.parseInt(value);
+            break;
+        case "time_per_move":
+            this.timePerMove = Integer.parseInt(value);
+            break;
+        case "player_names":
+            this.playerNames = value;
+            break;
+        case "your_bot":
+            this.yourBot = value;
+            break;
+        case "your_botid":
+            this.yourBotId = Integer.parseInt(value);
+            break;
+        case "field_width":
+            this.fieldWidth = Integer.parseInt(value);
+            break;
+        case "field_height":
+            this.fieldHeight = Integer.parseInt(value);
+            break;
+        }
+    }
+    
+    public void updateGame(String type, String value) {
+        switch (type) {
+        case "round":
+            this.gameRound = Integer.parseInt(value);
+            break;
+        case "field":
+            this.gameField = value;
+        }
+        System.out.println(type + " " + value);
+    }
+
+    public void runBot() {
+        while (scan.hasNextLine()) {
+            String line = scan.nextLine();
+
+            if (line.length() == 0) continue;
+
+            String[] command = line.split(" ");
+            switch (command[0]) {
+            case "settings":
+                this.updateSettings(command[2], command[3]);
+                break;
+
+            case "update":
+                this.updateGame(command[2], command[3]);
+                break;
+
+            case "action":
+                if (Integer.parseInt(command[3]) == this.yourBotId)
+                    System.out.println(this.getAction());
+                break;
+
+            }
+        }
+    }
+}
---
+++ Bots/JavaBots/TyrviBot.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class TyrviBot extends Bot {
+
+    private Scanner scan = new Scanner(System.in);
+
+    public String getAction() {
+        return "up";
+    }
+
+    public static void main(String[] args) {
+        Bot tyrvi = new TyrviBot();
+        int id = 2;
+        if (args.length > 0) {
+            id = Integer.parseInt(args[0]);
+        }
+        tyrvi.setBotId(id);
+        tyrvi.runBot();
+    }
+}
